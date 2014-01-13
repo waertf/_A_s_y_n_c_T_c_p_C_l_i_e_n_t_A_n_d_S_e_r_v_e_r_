3ff71c49fddd4ebfef475f39e9336d3038134975
nb <waertf.register@m2k.com.tw>
2014-01-13 14:13:57 +0800

    add function SendLocBackToWeb

--- ConsoleApplication2Tcp/AsyncTcpServer.cs
+++ ConsoleApplication2Tcp/AsyncTcpServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -89,11 +90,114 @@ namespace ConsoleApplication2Tcp
             byte[] b = BitConverter.GetBytes(a);
             return b.Reverse().ToArray();
         }
+        /**
+     * convert NMEA0183 latitude and longitude from degree minute, to degree
+     * i.e. ddmm.mmmm -> dd.dddddd
+     * @param f
+     * @return
+     * @auther Michael
+     * @since Aug 24, 2006
+     */
+        private float ConvertNmea0183ToUtm(float f)
+        {
+            int i = (int)(f / 100);
+            return (f / 100 - i) * 100 / 60 + i;
+        }
+
+        public byte[] SendLocBackToWeb(string recev)
+        {
+            char[] delimiterChars = { 'N','E','S','W'};
+            string[] tmp1 = recev.Split(delimiterChars);
+            float lat = ConvertNmea0183ToUtm(float.Parse(tmp1[1]));
+            float lon = ConvertNmea0183ToUtm(float.Parse(tmp1[2]));
+            byte[] latBytes = netduino.BitConverter.GetBytes(lat, netduino.BitConverter.ByteOrder.BigEndian);
+            byte[] lonBytes = netduino.BitConverter.GetBytes(lon, netduino.BitConverter.ByteOrder.BigEndian);
+            var m = new MemoryStream();
+            m.Write(lonBytes,0,lonBytes.Count());
+            m.Write(latBytes,0,latBytes.Count());
+            return m.ToArray();
+
+        }
         private string AddressOf(GCHandle handle)
         {
             IntPtr pointer = GCHandle.ToIntPtr(handle);
             return "0x" + pointer.ToString("X");
         }
 
+    }
+}
+
+namespace netduino
+{
+    public static class BitConverter
+    {
+        public static byte[] GetBytes(uint value)
+        {
+            return new byte[4] { 
+                    (byte)(value & 0xFF), 
+                    (byte)((value >> 8) & 0xFF), 
+                    (byte)((value >> 16) & 0xFF), 
+                    (byte)((value >> 24) & 0xFF) };
+        }
+
+        public static unsafe byte[] GetBytes(float value)
+        {
+            uint val = *((uint*)&value);
+            return GetBytes(val);
+        }
+
+        public static unsafe byte[] GetBytes(float value, ByteOrder order)
+        {
+            byte[] bytes = GetBytes(value);
+            if (order != ByteOrder.LittleEndian)
+            {
+                System.Array.Reverse(bytes);
+            }
+            return bytes;
+        }
+
+        public static uint ToUInt32(byte[] value, int index)
+        {
+            return (uint)(
+                value[0 + index] << 0 |
+                value[1 + index] << 8 |
+                value[2 + index] << 16 |
+                value[3 + index] << 24);
+        }
+
+        public static unsafe float ToSingle(byte[] value, int index)
+        {
+            uint i = ToUInt32(value, index);
+            return *(((float*)&i));
+        }
+
+        public static unsafe float ToSingle(byte[] value, int index, ByteOrder order)
+        {
+            if (order != ByteOrder.LittleEndian)
+            {
+                System.Array.Reverse(value, index, value.Length);
+            }
+            return ToSingle(value, index);
+        }
+
+        public enum ByteOrder
+        {
+            LittleEndian,
+            BigEndian
+        }
+
+        static public bool IsLittleEndian
+        {
+            get
+            {
+                unsafe
+                {
+                    int i = 1;
+                    char* p = (char*)&i;
+
+                    return (p[0] == 1);
+                }
+            }
+        }
     }
 }
