81dc0ed0b8cb96c5e713a8c39a362451666ecc24
nb <waertf.register@m2k.com.tw>
2014-01-16 14:42:40 +0800

    add filter library and modify code for it

--- WindowsFormsApplication1AVLSServer/Form1.Designer.cs
+++ WindowsFormsApplication1AVLSServer/Form1.Designer.cs
@@ -29,16 +29,17 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.GPSValid = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LatLon = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Speed = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Direction = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Temperature = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Event = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.GPSValid = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.DateTime = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.LatLon = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.Speed = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.Direction = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.Temperature = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.Status = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.Event = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.Message = new DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,97 +71,120 @@
             this.ID.HeaderText = "ID";
             this.ID.Name = "ID";
             this.ID.ReadOnly = true;
-            this.ID.Width = 42;
+            this.ID.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ID.Width = 57;
             // 
             // GPSValid
             // 
             this.GPSValid.HeaderText = "GPSValid";
             this.GPSValid.Name = "GPSValid";
             this.GPSValid.ReadOnly = true;
-            this.GPSValid.Width = 75;
+            this.GPSValid.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.GPSValid.Width = 90;
             // 
             // DateTime
             // 
             this.DateTime.HeaderText = "DateTime";
             this.DateTime.Name = "DateTime";
             this.DateTime.ReadOnly = true;
-            this.DateTime.Width = 75;
+            this.DateTime.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.DateTime.Width = 90;
             // 
             // LatLon
             // 
             this.LatLon.HeaderText = "Lat_Lon";
             this.LatLon.Name = "LatLon";
             this.LatLon.ReadOnly = true;
-            this.LatLon.Width = 70;
+            this.LatLon.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.LatLon.Width = 85;
             // 
             // Speed
             // 
             this.Speed.HeaderText = "Speed";
             this.Speed.Name = "Speed";
             this.Speed.ReadOnly = true;
-            this.Speed.Width = 58;
+            this.Speed.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Speed.Width = 73;
             // 
             // Direction
             // 
             this.Direction.HeaderText = "Direction";
             this.Direction.Name = "Direction";
             this.Direction.ReadOnly = true;
-            this.Direction.Width = 73;
+            this.Direction.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Direction.Width = 88;
             // 
             // Temperature
             // 
             this.Temperature.HeaderText = "Temperature";
             this.Temperature.Name = "Temperature";
             this.Temperature.ReadOnly = true;
-            this.Temperature.Width = 89;
+            this.Temperature.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Temperature.Width = 104;
             // 
             // Status
             // 
             this.Status.HeaderText = "Status";
             this.Status.Name = "Status";
             this.Status.ReadOnly = true;
-            this.Status.Width = 57;
+            this.Status.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Status.Width = 72;
             // 
             // Event
             // 
             this.Event.HeaderText = "Event";
             this.Event.Name = "Event";
             this.Event.ReadOnly = true;
-            this.Event.Width = 57;
+            this.Event.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Event.Width = 72;
             // 
             // Message
             // 
             this.Message.HeaderText = "Message";
             this.Message.Name = "Message";
             this.Message.ReadOnly = true;
+            this.Message.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Message.Width = 69;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 336);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(713, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(713, 358);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn GPSValid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DateTime;
-        private System.Windows.Forms.DataGridViewTextBoxColumn LatLon;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Speed;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Direction;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Temperature;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Event;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn ID;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn GPSValid;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn DateTime;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn LatLon;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn Speed;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn Direction;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn Temperature;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn Status;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn Event;
+        private DataGridViewAutoFilter.DataGridViewAutoFilterTextBoxColumn Message;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- WindowsFormsApplication1AVLSServer/Form1.cs
+++ WindowsFormsApplication1AVLSServer/Form1.cs
@@ -4,19 +4,89 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using DataGridViewAutoFilter;
 
 namespace WindowsFormsApplication1AVLSServer
 {
+    
     public partial class Form1 : Form
     {
+        private ToolStripStatusLabel filterStatusLabel, showAllLabel;
+        private Thread t1;
         public Form1()
         {
             InitializeComponent();
-            dataGridView1.ColumnHeaderMouseClick += new DataGridViewCellMouseEventHandler(dataGridView1_ColumnHeaderMouseClick);
+            
+
             dataGridView1.DataBindingComplete += new DataGridViewBindingCompleteEventHandler(dataGridView1_DataBindingComplete);
+            this.Shown += new EventHandler(Form1_Shown);
+            dataGridView1.AutoSize = true;
+            dataGridView1.BindingContextChanged += new EventHandler(dataGridView1_BindingContextChanged);
+
+            filterStatusLabel = new ToolStripStatusLabel();
+            showAllLabel = new ToolStripStatusLabel();
+            statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            filterStatusLabel,showAllLabel});
+            filterStatusLabel.Text = "";
+            filterStatusLabel.Visible = false;
+            showAllLabel.Text = "Show &All";
+            showAllLabel.Visible = false;
+            showAllLabel.IsLink = true;
+            showAllLabel.LinkBehavior = LinkBehavior.HoverUnderline;
+
+            showAllLabel.Click += new EventHandler(showAllLabel_Click);
+            
+
+            #region test
+             t1 = new System.Threading.Thread
+              (delegate()
+              {
+                  Random rand = new Random();
+                  while (true)
+                  {
+                      string test = dataGridView1.RowCount.ToString();
+                      this.UIThread(() => this.Text = "Total number : "+test);
+                      Thread.Sleep(100);
+                  }
+              });
+            t1.Start();
+            #endregion test
+
         }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            t1.Abort();
+            base.OnFormClosing(e);
+        }
+        private void showAllLabel_Click(object sender, EventArgs e)
+        {
+            DataGridViewAutoFilterTextBoxColumn.RemoveFilter(dataGridView1);
+        }
+
+        void dataGridView1_BindingContextChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.DataSource == null) return;
+
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.HeaderCell = new
+                    DataGridViewAutoFilterColumnHeaderCell(col.HeaderCell);
+            }
+            dataGridView1.AutoResizeColumns();
+        }
+
+        void Form1_Shown(object sender, EventArgs e)
+        {
+            Size size = new Size(dataGridView1.Width, dataGridView1.Height);
+            this.Size = size;
+        }
+
+        
 
         void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
         {
@@ -25,40 +95,39 @@ namespace WindowsFormsApplication1AVLSServer
             {
                 column.SortMode = DataGridViewColumnSortMode.Programmatic;
             }
-        }
-
-        void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
-        {
-            DataGridViewColumn newColumn = dataGridView1.Columns[e.ColumnIndex];
-            DataGridViewColumn oldColumn = dataGridView1.SortedColumn;
-            ListSortDirection direction;
-
-            // If oldColumn is null, then the DataGridView is not sorted.
-            if (oldColumn != null)
+            String filterStatus = DataGridViewAutoFilterColumnHeaderCell
+          .GetFilterStatus(dataGridView1);
+            if (String.IsNullOrEmpty(filterStatus))
             {
-                // Sort the same column again, reversing the SortOrder.
-                if (oldColumn == newColumn &&
-                    dataGridView1.SortOrder == SortOrder.Ascending)
-                {
-                    direction = ListSortDirection.Descending;
-                }
-                else
-                {
-                    // Sort a new column and remove the old SortGlyph.
-                    direction = ListSortDirection.Ascending;
-                    oldColumn.HeaderCell.SortGlyphDirection = SortOrder.None;
-                }
+                showAllLabel.Visible = false;
+                filterStatusLabel.Visible = false;
             }
             else
             {
-                direction = ListSortDirection.Ascending;
-            }
+                showAllLabel.Visible = true;
+                filterStatusLabel.Visible = true;
+                filterStatusLabel.Text = filterStatus;
 
-            // Sort the selected column.
-            dataGridView1.Sort(newColumn, direction);
-            newColumn.HeaderCell.SortGlyphDirection =
-                direction == ListSortDirection.Ascending ?
-                SortOrder.Ascending : SortOrder.Descending;
+            }
+        }
+    }
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Executes the Action asynchronously on the UI thread, does not block execution on the calling thread.
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="code"></param>
+        public static void UIThread(this Control @this, Action code)
+        {
+            if (@this.InvokeRequired)
+            {
+                @this.BeginInvoke(code);
+            }
+            else
+            {
+                code.Invoke();
+            }
         }
     }
 }
