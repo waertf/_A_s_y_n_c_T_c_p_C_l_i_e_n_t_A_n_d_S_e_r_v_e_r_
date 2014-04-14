4c5553ac2ef0a1ee94ac1d3f4aa5144783e506a9
nb <waertf.register@m2k.com.tw>
2014-04-14 12:14:18 +0800

    set log maxsize=500MB

--- AVLSServer/Program.cs
+++ AVLSServer/Program.cs
@@ -77,7 +77,7 @@ namespace AVLSServer
             #endregion catchCloseEvent
             SiAuto.Si.Enabled = true;
             SiAuto.Si.Level = Level.Debug;
-            SiAuto.Si.Connections = @"file(filename=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log.sil,rotate=weekly,append=true,maxparts=5)";
+            SiAuto.Si.Connections = @"file(filename=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log.sil,rotate=weekly,append=true,maxparts=5,maxsize=500MB)";
             SiAuto.Main.LogText(Level.Debug, "waiting for connect", "");
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             port7000reconnect = true;
