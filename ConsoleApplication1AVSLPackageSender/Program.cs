e09845720c49212985f66f93fc9775ebd7130b9c
alonso <alonso@wavegis.com.tw>
2014-05-28 16:40:24 +0800

    get lat lon from csv file then send to avls function add

--- ConsoleApplication1AVSLPackageSender/Program.cs
+++ ConsoleApplication1AVSLPackageSender/Program.cs
@@ -58,6 +58,7 @@ namespace ConsoleApplication1AVSLPackageSender
         static TcpClient avlsTcpClient;
         static void Main(string[] args)
         {
+            /*
             string path = Environment.CurrentDirectory+"\\"+"sd.equipment.txt";
             if (File.Exists(path))
             {
@@ -70,11 +71,90 @@ namespace ConsoleApplication1AVSLPackageSender
                 Console.ReadLine();
                 return;
             }
+            */
+            uidStrings = GetAllFilesCSV(Environment.CurrentDirectory).ToArray();
             avlsTcpClient = new TcpClient(ConfigurationManager.AppSettings["ServerIP"], 7000);
             networkStream = avlsTcpClient.GetStream();
-            Thread sendthread = new Thread(() => send(networkStream));
-            sendthread.Start();
-            
+            //Thread sendthread = new Thread(() => send(networkStream));
+            //sendthread.Start();
+
+            for (int i = 0; i < uidStrings.Length; i++)
+            {
+                int i1 = i;
+                /*
+                var workToDo = new WaitCallback(o =>
+                  {
+                    // Your stuff here
+                      SendByUid(uidStrings[i1], networkStream);
+                  });
+                ThreadPool.QueueUserWorkItem(workToDo);
+                */
+                Thread sendUidThread = new Thread(delegate()
+                {
+                    SendByUid(uidStrings[i1], networkStream);
+                });
+                sendUidThread.Start();
+            }
+            Console.WriteLine("press any key to exist...");
+            Console.ReadLine();
+        }
+
+        private static void SendByUid(string uid, NetworkStream networkStream)
+        {
+            string path = Environment.CurrentDirectory + "\\" + uid + ".csv";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    //Console.WriteLine(sr.ReadLine());
+                    string[] oneLineStrings = sr.ReadLine().Split(new char[]{','});
+                    string lon = oneLineStrings[1];
+                    string lat = oneLineStrings[2];
+                    ConvertLocToAvlsLoc(ref lat, ref lon);
+                    string loc = "N" + lat + "E" + lon + ",";
+                    string time = DateTime.UtcNow.ToString("yyMMddHHmmss");
+                    string Speed = rand.Next(0, 1000).ToString();
+                    string Dir = rand.Next(0, 360).ToString();
+                    string gps = GPSValid[rand.Next(0, GPSValid.Length)];
+                    string _event = Event[rand.Next(0, Event.Length)];
+                    string message = Msg[rand.Next(0, Msg.Length)];
+                    string package = "%%" + uid + "," +
+                              gps + "," +
+                              time + "," +
+                              loc + "," +
+                              Speed + "," +
+                              Dir + "," +
+                              Temp + "," +
+                              Status + "," +
+                              _event + "," +
+                              message + Environment.NewLine;
+                    if (bool.Parse(ConfigurationManager.AppSettings["debugMsg"]))
+                    {
+                        Console.WriteLine(package);
+                    }
+                    byte[] sendByte = Encoding.UTF8.GetBytes(package);
+
+                    if (true)
+                    {
+                        try
+                        {
+                            networkStream.Write(sendByte, 0, sendByte.Length);
+                            //networkStream.BeginWrite(sendByte, 0, sendByte.Length, sendAsyncCallback, networkStream);
+                            networkStream.Flush();
+                        }
+                        catch (Exception ex)
+                        {
+                            networkStream.Close();
+                            avlsTcpClient.Close();
+                            avlsTcpClient = new TcpClient(ConfigurationManager.AppSettings["ServerIP"], 7000);
+                            networkStream = avlsTcpClient.GetStream();
+                            Thread sendthread = new Thread(() => send(networkStream));
+                            sendthread.Start();
+                        }
+                    }
+                    Thread.Sleep(int.Parse(ConfigurationManager.AppSettings["sendSleepTime"]));
+                }
+            }
         }
 
         static  object send(NetworkStream networkStream)
@@ -135,6 +215,10 @@ namespace ConsoleApplication1AVSLPackageSender
             }
         }
 
+        static List<String> GetAllFilesCSV(String directory)
+        {
+            return Directory.GetFiles(directory, "*.csv", SearchOption.AllDirectories).Select(f => Path.GetFileNameWithoutExtension(f)).ToList();
+        }
         private static void sendAsyncCallback(IAsyncResult ar)
         {
             try
